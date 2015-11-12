5830c5ff5288bcb58875552b5d9a6cf48035f9dd
Colin Cross <ccross@android.com>
2015-11-11 17:55:23 -0800

    Adding binaries for master-eda391df8e36
    Changes since v1.6.0:
    eda391df8e36 Print status when edge finishes on dumb terminals
    c340ca129272 Merge tag 'v1.6.0' into master
    d99e75e05590 Initial empty repository
    
    Change-Id: I228b79c6c77355b408df7b91fef24032c4dc4422

--- build-ninja.sh
+++ build-ninja.sh
@@ -4,7 +4,8 @@
 # leaves output in /tmp/prebuilts/ninja/$OS-x86/
 
 PROJ=ninja
-VER=1.6.0
+VER=master
+BASE_VER=v1.6.0
 MSVS=2013
 
 source $(dirname "$0")/build-common.sh build-common.sh
@@ -14,9 +15,12 @@ export PATH="$PATH":.
 
 # ninja specific steps
 cd $RD
-git clone https://github.com/martine/ninja.git src
+git clone https://android.googlesource.com/platform/external/ninja.git src
 cd src
-git checkout v$VER
+git remote add upstream https://github.com/martine/ninja.git
+git fetch upstream
+git checkout $VER
+INSTALL_VER=${INSTALL_VER/${VER}/${VER}-$(git rev-parse --short=12 HEAD)}
 if [[ "$OS" == "windows" ]] ; then
 	 PLATFORM="--platform=msvc"
 fi
@@ -24,5 +28,8 @@ fi
 
 # install
 cp $RD/src/ninja $INSTALL
+
+EXTRA_FILE="LICENSE MODULE_LICENSE_APL"
+EXTRA_COMMIT_MSG=$(echo -e "\n\nChanges since ${BASE_VER}:" && git log --oneline --abbrev=12 ${BASE_VER}..HEAD)
 
 commit_and_push
